485d50f7f57541768c094dfb3c3c9ee2ce1f9adc
Jasper De Smet <jasp.desmet@gmail.com>
2015-11-13 18:09:55 +0100

    List styling

---
+++ public/drupal/sites/all/themes/areau15/templates/node--item.tpl.php
@@ -0,0 +1,8 @@
+<div class="item">
+	<div class="overlay">
+		<h3><?php echo $title; ?></h3>
+		<div class="description"><?php echo $desicription; ?></div>
+		<div class="footer areau-credits"><p class="line2">out-and-out the inside</p><p class="line1">Design - Interior - Services</p></div>
+	</div>
+	<?php print render($field_image); ?>
+</div>
