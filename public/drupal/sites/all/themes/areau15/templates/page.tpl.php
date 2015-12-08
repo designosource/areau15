d96fcfee5aa488d68f51110dbf0e556ccda8387f
Jasper De Smet <jasp.desmet@gmail.com>
2015-12-08 15:56:36 +0100

    fixed responsive footer

--- public/drupal/sites/all/themes/areau15/templates/page.tpl.php
+++ public/drupal/sites/all/themes/areau15/templates/page.tpl.php
@@ -11,14 +11,26 @@
 
             <?php if ($main_menu): ?>
                 <nav class="wrapper" role="navigation" tabindex="-1">
-                    <?php
-                    // This code snippet is hard to modify. We recommend turning off the
-                    // "Main menu" on your sub-theme's settings form, deleting this PHP
-                    // code block, and, instead, using the "Menu block" module.
-                    // @see https://drupal.org/project/menu_block
-                    print theme('links__system_main_menu', array(
-                        'links' => $main_menu
-                    )); ?>
+                    <ul class="mobile">
+                        <li>
+                            <a href="/" title="" class="front-nav visible-sm visible-xs">Front</a>
+                        </li>
+                        <li>
+                            <button id="menu-button">MENU</button>
+                        </li>
+                    </ul>
+
+                    <div class="main-menu-wrapper" id="primary-menu">
+                        <?php
+                        // This code snippet is hard to modify. We recommend turning off the
+                        // "Main menu" on your sub-theme's settings form, deleting this PHP
+                        // code block, and, instead, using the "Menu block" module.
+                        // @see https://drupal.org/project/menu_block
+                        print theme('links__system_main_menu', array(
+                            'links' => $main_menu,
+                            'id' => 'main-menu'
+                        )); ?>
+                    </div>
                 </nav>
             <?php endif; ?>
 
