bc0f6c4a0ff734f96814d8d46c83c94c2b7b595f
Guades <ianemsens@gmail.com>
2015-12-15 17:45:06 +0100

    final final commit

--- public/drupal/sites/all/themes/areau15/templates/views-view-fields--menu_view.tpl.php
+++ public/drupal/sites/all/themes/areau15/templates/views-view-fields--menu_view.tpl.php
@@ -1,23 +1,24 @@
 <?php
 	$title = $row->_field_data['nid']['entity']->title;
-	$title = $row->_field_data['nid']['entity']->title;
 	$description = $row->_field_data['nid']['entity']->body['und'][0]['value'];
 	$image = [
 		'path' => '/sites/default/files/menu/' . $row->_field_data['nid']['entity']->field_image['und'][0]['filename']
 	];
 ?>
 
-<div class="item">
-<?php if ($title !== "null"): ?>
-	<div class="overlay">
-		<h3><?php echo($title); ?></h3>
-		<h4>To-Style & Template</h4>
-		<div class="description"><?php echo($description); ?></div>
-		<div class="footer areau-credits">
-			<p class="line2">out-and-out the inside</p>
-			<p class="line1">Design - Interior - Services</p>
+<a href="/<?php echo strToLower($title); ?>">
+	<div class="item">
+	<?php if ($title !== "null"): ?>
+		<div class="overlay">
+			<h3><?php echo($title); ?></h3>
+			<h4>To-Style & Template</h4>
+			<div class="description"><?php echo($description); ?></div>
+			<div class="footer areau-credits">
+				<p class="line2">out-and-out the inside</p>
+				<p class="line1">Design - Interior - Services</p>
+			</div>
 		</div>
+	<?php endif ?>
+		<?php echo theme_image($image); ?>
 	</div>
-<?php endif ?>
-	<?php echo theme_image($image); ?>
-</div>
+</a>
