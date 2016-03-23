8b5fe5ff552d3e154c2e420322299fa42faf5ca6
Guades <ianemsens@gmail.com>
2016-03-23 21:49:25 +0100

    final final final final commit, if it ain't I'm rewriting everything in a week fuck this

---
+++ public/drupal/sites/all/themes/areau15/templates/views-view--ons_verhaal.tpl.php
@@ -0,0 +1,89 @@
+<section>
+	<img src="sites/all/themes/areau15/images/verhaal/01.png" alt="ons-verhaal 1">
+	<div class="text first white">
+		<p class="text-center">
+			INTERIEURARCHITECTEN<br>WOUTER MEGANCK, ANKE KLÜTTERMANN, CELINE POISSONNIER VORMEN AREAU TOT  EEN STUDIE- EN ONTWERPBUREAU VOOR INTERIEUR, OBJECTEN EN INSTALLATIES. 
+		</p>
+		<b class="text-center">ONS VERHAAL</b>
+		<b class="text-center ssp">Weten waar AREAU de mosterd haalt</b>
+		<p>
+			ELK GEBOUW EN ZIJN BINNENKANT HEEFT EEN EIGEN VERHAAL. ELKE BEWONER OOK. AREAU SMELT PLAATSEN EN PERSONAGES SAMEN IN EEN SCENOGRAFIE. IN EEN CONTEXT DIE SPREEKT: TOT DE VERBEELDING, TOT DE BUITENWERELD, TOT U. IN EEN CONTEXT DIE OOR HEEFT: NAAR UW VERLEDEN, NAAR UW TOEKOMST, NAAR U. IN EEN CONTEXT DIE OOG HEEFT: VOOR ESTHETIEK, VOOR DETAIL, VOOR U. STAPT U MEE IN ONS VERHAAL?
+		</p>
+		<b class="text-center ssp top-150">Waar de grens van het interieur<br>begint en eindigt</b>
+		<p>
+			DE BINNENKANT ZEGT IETS OVER MENSEN. ONTHULT EEN IDENTITEIT. EEN SUBTIELE SCHOONHEID. EEN SPREKEND KARAKTER. AREAU INTERPRETEERT UW WENSEN EN TEKENT ZE UIT TOT EEN CONCEPT. EEN UITERLIJKE VORM VAN WAT IN UW BINNENSTE HUIST. EEN ONTWERP MET EEN ZIEL. EEN VERHAAL: ONS VERHAAL.
+		</p>
+	</div>
+</section>
+<section>
+	<img src="sites/all/themes/areau15/images/verhaal/02.png" alt="ons-verhaal 2">
+	<div class="text second">
+		<b class="text-center">DESIGN - INTERIOR - SERVICES</b>
+		<b class="text-center ssp bot-20">out - and - out the inside</b>
+		<b class="top-30">DESIGN</b>
+		<b class="ssp">/di’zajn/</b>
+		<p class="top-10">
+			1 artistiek ontwerp, zowel van iconische designers als van eigen hand 2 stijl, vormgeving en (kunst)geschiedenis 3 doordacht vormgegeven voorwerpen om een ruimte in te richten
+		</p>
+		<b class="top-30">INTERIOR</b>
+		<b class="ssp">/InˈtIərIə/</b>
+		<p class="top-10">
+			1 artistiek ontwerp, zowel van iconische designers als van eigen hand 2 stijl, vormgeving en (kunst) geschiedenis 3 doordacht vormgegeven voorwerpen om een ruimte in te richten
+		</p>
+		<b class="top-30">SERVICES</b>
+		<b class="ssp">/’s3:vIs/</b>
+		<p class="top-10">
+			1 klanten helpen, inspireren en adviseren 2 coördineren, samenwerken en overtuigen 3 luisterend oor, neus voor esthetiek, oog voor detail
+		</p>
+		<b class="top-30">AREAU</b>
+		<b class="ssp">\a.  o\/</b>
+		<p class="top-10">
+			1 design 2 interior 3 services
+		</p>
+	</div>
+</section>
+<section>
+	<img src="sites/all/themes/areau15/images/verhaal/03.png" alt="ons-verhaal 3">
+	<div class="text third white">
+		<b class="text-center top-30">PROJECTEN</b>
+		<p>
+			AREAU DEELT PANDEN OP IN AUTHENTIEKE WOONEENHEDEN. NIET IDENTIEK, WEL MET EEN EIGEN IDENTITEIT. HEDEN, VERLEDEN. TOEKOMST, GESCHIEDENIS. INNOVATIEF OF TRADITIONEEL VAKMANSCHAP. ELK ONTWERP TAST GRENZEN EN CONTRASTEN AF, FLIRT MET SCHOONHEID EN FUNCTIONALITEIT. BESTAANDE PANDEN KRIJGEN EEN NIEUW VERHAAL. GELEEFDE GEBOUWEN WORDEN TEN VOLLE BENUT. DE BINNENKANT WORDT HERTEKEND TOT IN DE KLEINSTE DETAILS. KLEINERE RUIMTES WORDEN OPTIMAAL ONTGONNEN EN RUIMTELIJKER BELEEFD.  
+		</p>
+		<b class="text-center ssp">Hercomponeren van woonentiteiten<br>met een creatieve ziel</b>
+		<p>
+			ALLES BEWEEGT, INTERAGEERT EN EVOLUEERT: DE FUNCTIE VAN GEBOUWEN, MAAR NET ZO GOED UW WENSEN ALS UW LEEFPATROON. U WILT INSTAPKLARE LEVENSKWALITEIT? U WILT MEER RUIMTE, KARAKTER EN FLEXIBILITEIT? UW WILT INDIVIDUALITEIT IN EEN COLLECTIEF GEHEEL? AREAU BENUT ELKE RUIMTE OPTIMAAL. EN U, U BELEEFT ELKE RUIMTE TEN VOLLE.
+		</p>
+	</div>
+</section>
+<section>
+	<img src="sites/all/themes/areau15/images/verhaal/04.png" alt="ons-verhaal 4">
+	<div class="text fourth white">
+		<b class="text-center top-30">INTERIEURARCHITECTUUR</b>
+		<p>
+			STUDIE, ADVIES EN CONCEPT. RESPECT VOOR AUTHENTICITEIT EN KWALITEIT. DESIGN TOT IN DE KLEINSTE DETAILS. AREAU LEEST DE CONTEXT VAN U EN UW HUIS, EN TEKENT DIE VERHAALLIJN UIT TOT EEN CREATIEF ONTWERP. MET KENNIS VAN ZAKEN, MET PASSIE, MET VAKMANSCHAP. EN MET UW INBRENG.
+		</p>
+		<b class="text-center ssp top-20">Bezield scheppen en realiseren<br>van uw leefentiteit</b>
+		<p class="top-20">
+			AREAU RESPECTEERT HET VERHAAL VAN UW GEBOUW, EN HOUDT BIJZONDERE BOUWSTENEN IN EER. ELK ELEMENT VERTELT EEN VERHAAL, STRAALT PERFECTIE UIT. ELK MEUBEL, DETAIL OF KUNSTWERK IS BEWUST GEKOZEN: VAN ICONISCHE MEESTERWERKEN TOT VERRASSENDE DESIGNS OF EIGEN ONTWERPEN. SUBTIEL VERPAKTE LUXE WAAR U TROTS OP BENT.
+		</p>
+	</div>
+</section>
+<section>
+	<img src="sites/all/themes/areau15/images/verhaal/05.png" alt="ons-verhaal 5">
+	<div class="text fifth brown">
+		<b class="text-center">KUNST + VORM</b>
+		<p class="top-20">
+			INTERIEUR IS MEER DAN RUIMTES VORMGEVEN. AREAU IS MEER DAN INTERIEUR VORMGEVEN. KUNST IS MEER DAN VORM. HET IS DE KUNST OM MEER TE WILLEN, VERDER TE GAAN, GRENZEN AF TE TASTEN. AREAU TEKENT VOOR DIE KUNST.
+		</p>
+		<b class="text-center ssp top-20">Kunst vormt ons. Wij vormen kunst.</b>
+		<p class="top-20">
+			AREAU BLIJFT NIET BINNEN DE LIJNEN VAN HET INTERIEURONTWERP. WIJ TREDEN NAAR BUITEN MET KUNST EN VORM. WANT ONTWERPEN GAAT VERDER DAN WAT ER OP PAPIER STAAT. WIJ DAGEN ONSZELF UIT OM TE GROEIEN: BINNEN EN BUITEN ONZE COMFORTZONE.
+		</p>
+		<b class="text-center ssp top-20">Wat doet AREAU om in kunstvorm te blijven?</b>
+		<p class="text-center ssp top-20">
+			STUDIE EN ONDERZOEK / PUBLICATIES / OPENHUISDAGEN / TENTOONSTELLINGEN EN EXPO’S / EIGEN INSTALLATIES EN EVENTS / ONTWERP- EN DESIGNWEDSTRIJDEN
+		</p>
+		<b class="text-center top-150">AREAU INTERIEURARCHITECTEN</b>
+		<p class="text-center">Wouter Meganck, Anke Klüttermann, Celine Poissonnier</p>
+	</div>
+</section>
